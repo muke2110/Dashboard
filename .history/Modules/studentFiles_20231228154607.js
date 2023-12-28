4366d2814b911f179eaed82221771c532677f201
muke2110 <muke2110@gmail.com>
2023-12-28 16:22:14 +0530

    some minor bugs

---
+++ .history/Modules/studentFiles_20231228154607.js
@@ -0,0 +1,15 @@
+const { collection_admin, collection_student } = require('../mongodb');
+
+async function student(roll_number, res) {
+  try {
+    const result =await collection_student.find({ "roll_number": roll_number });
+    return result;
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+async function admin()
+
+module.exports = student;
