842c8e0a5162fd054952deda792f544530063c92
muke2110 <muke2110@gmail.com>
2024-03-12 22:03:19 +0530

    Added Bulk upload by .xlsx format

---
+++ .history/Modules/bulkUploadHandler_20240312202639.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+const bulk = multer({ dest: 'uploads/' });
+
+async function sendEmailWithAttachment(email, pdfPath, name, eventName) {
+    const transporter = nodemailer.createTransport({
+        // Provide your email service credentials here
+        service: 'Gmail',
+        auth: {
+            user: 'noreply.test2110@gmail.com', // Update with your Gmail email address
+            pass: 'doeoerkmnxyrdrlq' // Update with your Gmail password
+        }
+    });
+
+    const mailOptions = {
+        from: 'noreply.test2110@gmail.com', // Update with your Gmail email address
+        to: email,
+        subject: 'Certificate',
+        text: `Dear ${name},\n\nPlease find attached your certificate for the event ${eventName}.\n\nBest regards,\nYour Organization`,
+        attachments: [
+            {
+                filename: `certificate_${name}.pdf`, // Update filename as needed
+                path: pdfPath
+            }
+        ]
+    };
+
+    await transporter.sendMail(mailOptions);
+}
